86f0944d0b9f2ee706ef2e25c8109a453e1300f6
elahi68 <shaikelahi68@gmail.com>
2020-07-09 18:54:38 +0530

    Things you should know about python and Linux shell

---
+++ firstFile.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+print("This is a test and this is executed from Command line of linux/Shell of Linux");
+print("But not from the command line of Python shhell");
+print("By executing the command ./filename.py this test description will show up");
+print("Hence it is to prove that this is a just like bash command but with different set of");
+print("command and programs");
+print("Find the executable of python3 that will be in /usr/bin/python3")
+print("And tell the shell to go to that dir and thre will be python3 executable and execute that with this location")
+print("Bam!! Done : ~")
+print("One more thing change the permissions to x or 0 or executable by chmod +x or chmod 777 filename.py")
+print("If you want to execute from shell another alternate we all know do execute >python3 filename.py")
